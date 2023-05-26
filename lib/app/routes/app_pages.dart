15f82b7bd58b695d4272c40ae2d4d48a72d66431
NOGUEN <nhg1113@naver.com>
2023-05-26 16:40:50 +0900

    Test Settings

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+part './app_routes.dart';
+
+class AppPages {
+  /*
+  static final pages = [
+    GetPage(
+        name: Routes.INITIAL, page: () => HomePage(), binding: HomeBinding()),
+    GetPage(
+        name: Routes.DETAILS,
+        page: () => DetailsPage(),
+        binding: DetailsBinding()),
+  ];
+  */
+}
