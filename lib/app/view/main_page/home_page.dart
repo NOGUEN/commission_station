2247ac84e7a65b1aec5fc0a1258f78259167be75
NOGUEN <nhg1113@naver.com>
2023-11-22 20:54:39 +0900

    feat: 배너 페이지 연결

--- lib/app/view/main_page/home_page.dart
+++ lib/app/view/main_page/home_page.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:commission_station/app/controller/main/home_page_controller.dart';
 import 'package:commission_station/app/core/base/base_view.dart';
+import 'package:commission_station/app/view/common/commission_station_banner.dart';
 import 'package:commission_station/app/view/common/commission_station_webview_card.dart';
+import 'package:commission_station/app/view/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -86,23 +88,48 @@ class _BannerDelegate extends SliverPersistentHeaderDelegate {
           bannerController.currentPage.value = page;
         },
         children: [
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Text('배너 1'),
-            ),
+          const CSBanner(
+            title: '고라니 작가의 커미션!',
+            subTitle: '고라니가 그려주는 그림!',
+            assetRoute: 'images/banner_1.png',
+            fontColor: AppColors.black,
           ),
-          Container(
-            color: Colors.green,
-            child: Center(
-              child: Text('배너 2'),
-            ),
+          const CSBanner(
+            title: '비둘기 작가의 커미션!',
+            subTitle: '비둘기가 쪼아주는 그림!',
+            assetRoute: 'images/banner_2.png',
+            fontColor: AppColors.white,
           ),
-          Container(
-            color: Colors.orange,
-            child: Center(
-              child: Text('배너 3'),
-            ),
+          Stack(
+            children: [
+              Container(
+                width: 360.w,
+                child: Image.asset(
+                  'images/banner_1.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16.h, left: 16.w),
+                child: Text(
+                  '고라니 작가의 커미션!',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 35.h, left: 16.w),
+                child: Text(
+                  '고라니가 그려주는 그림!',
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -153,4 +180,38 @@ class BannerController extends GetxController {
     pageController.dispose();
     super.onClose();
   }
+}
+
+Widget bannerBuilder(Strin) {
+  return Stack(
+    children: [
+      Container(
+        width: 360.w,
+        child: Image.asset(
+          'images/banner_1.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 16.h, left: 16.w),
+        child: Text(
+          '고라니 작가의 커미션!',
+          style: TextStyle(
+            fontSize: 18.sp,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 35.h, left: 16.w),
+        child: Text(
+          '고라니가 그려주는 그림!',
+          style: TextStyle(
+            fontSize: 14.sp,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    ],
+  );
 }
